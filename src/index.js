21d10932eae54b950f6be3a2ed35d2540c5f5b8b
Rahul Tiwari <rt699652@gmail.com>
2025-10-25 18:38:04 +0530

    some-update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { store } from "./components/store";
+import { store } from "./components/Store";
 
 ReactDOM.render(
   <Provider store={store}>

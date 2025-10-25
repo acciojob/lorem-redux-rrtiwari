de67ed8e3fe749477a2c93fe26d547a668d2fd7d
Rahul Tiwari <rt699652@gmail.com>
2025-10-25 18:51:49 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -13,14 +13,17 @@ const App = () => {
 
   return (
     <div>
-      <h1>A short Naration of Lorem Ipsum</h1>
+      <h1>
+        Below Contains A title and Body gotten froma random API, Please take
+        your time to Review
+      </h1>
       {loading && <h4>Loading...</h4>}
       {error && <h4>{error}</h4>}
       <ul>
         {content.map((post) => (
           <li key={post.id}>
-            <h4 className="title">Title :sunt aut facere repellat provident occaecati excepturi optio reprehenderit</h4>
-            <p className="body">{post.body}</p>
+            <h4 className="title">Title :{post.title}</h4>
+            <p>Body :{post.body}</p>
           </li>
         ))}
       </ul>

60da427d80665314d1a31d919f4b6f2dd9685d8d
Rahul Tiwari <rt699652@gmail.com>
2025-10-25 19:36:12 +0530

    updated

--- src/components/LoremSlice.js
+++ src/components/LoremSlice.js
@@ -2,19 +2,42 @@ import "regenerator-runtime/runtime";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchPosts = createAsyncThunk("lorem/fetchPosts", async () => {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=5"
-  );
-  const data = await response.json();
-  await new Promise((resolve) => setTimeout(resolve, 1000)); 
-  return data;
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return [
+    {
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit suscipit recusandae consequuntur expedita et cum",
+    },
+    {
+      id: 2,
+      title: "qui est esse",
+      body: "est rerum tempore vitae sequi sint nihil reprehenderit dolor",
+    },
+    {
+      id: 3,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure voluptatem occaecati omnis eligendi aut",
+    },
+    {
+      id: 4,
+      title: "eum et est occaecati",
+      body: "ullam et saepe reiciendis voluptatem adipisci",
+    },
+    {
+      id: 5,
+      title: "nesciunt quas odio",
+      body: "repudiandae veniam quaerat sunt sed alias aut fugiat sit autem",
+    },
+  ];
 });
 
 const loremSlice = createSlice({
   name: "lorem",
   initialState: {
     posts: [],
-    loading: true,
+    loading: false,
     error: null,
   },
   reducers: {},
